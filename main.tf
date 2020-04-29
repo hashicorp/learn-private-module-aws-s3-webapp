731689e5f2c17efdd7274f3be0207f393b974402
Rachel Sharp <rachel@hashicorp.com>
2020-04-29 13:54:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,38 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.prefix}-${var.name}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prefix}-${var.name}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
 resource "aws_s3_bucket_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
-  bucket       = var.bucket
+  bucket       = aws_s3_bucket.bucket.id
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
 
