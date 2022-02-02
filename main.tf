78c7154146af0d441079fb7f72856b3484cda9b9
Robin Norwood <rnorwood@hashicorp.com>
2022-02-02 12:12:22 -0600

    Update 'aws_s3_bucket_object' to 'aws_s3_object'

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_policy" "policy" {
 EOF
 }
 
-resource "aws_s3_bucket_object" "webapp" {
+resource "aws_s3_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
