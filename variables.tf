49bf601b2310243e34cbf1f3c78251a09b520181
pekoraaaa <73930188+god1097@users.noreply.github.com>
2022-04-27 18:19:25 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
-variable "region" {}
-variable "prefix" {}
-variable "name" {}
+variable "region" {
+  description = "This is the cloud hosting region where your webapp will be deployed."
+}
+
+variable "prefix" {
+  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+}
+
+variable "name" {
+  description = "Your name to attach to the webapp address"
+}
