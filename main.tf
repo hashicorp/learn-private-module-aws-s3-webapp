2185f31c74114f1f9b82e2317c4a4679005ff45e
pekoraaaa <73930188+god1097@users.noreply.github.com>
2022-04-27 17:53:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,14 @@ provider "aws" {
   region = var.region
 }
 
+module "s3-webapp" {
+  source  = "app.terraform.io/cc10-test/s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+}
+  
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.prefix}-${var.name}"
 
