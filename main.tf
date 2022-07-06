13150e67d0c71c55df8beac3858fa55f40f6252e
Robin Norwood <rnorwood@hashicorp.com>
2022-07-06 11:25:32 -0500

    Don't include terraform or provider blocks in module

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.prefix}-${var.name}"
 
