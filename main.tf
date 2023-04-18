8bfc537a704f4a56da4778a09a25181b36dee967
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-04-18 21:31:00 +1000

    Made edits to accomodate for new changes to AWS S3 Buckets

--- main.tf
+++ main.tf
@@ -13,6 +13,18 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket" {
@@ -28,6 +40,10 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
 }
 
 resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.example,
+    aws_s3_bucket_ownership_controls.example,
+  ]
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
