f9cb4df443bb06304200070c38f8cd021947ca39
mohamedhelkinz <90804026+mohamedhelkinz@users.noreply.github.com>
2024-02-04 15:21:27 +0100

    add modification

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = aws_s3_bucket.bucket.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
